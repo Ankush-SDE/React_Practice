8faf5b5af35d839667134bf785551c04671e6a45
Ankush Kumar <ankush1524kumar@gmail.com>
2026-06-26 20:08:50 +0530

    add Comment form

--- src/CommnetsForm.jsx
+++ src/CommnetsForm.jsx
@@ -1,16 +1,40 @@
+import { useState } from "react"
+
 export default function CommentForm(){
+  let [formDate,setFormDate] = useState({
+    username:"",
+    remarks:"",
+    rating:5
+  });
+  let handleInputChange = (event)=>{
+    setFormDate((currDate)=>{
+      return{
+        ...currDate,
+        [event.target.name]:event.target.value,
+      };
+    });
+  };
+  let handleSubmit =(event)=>{
+    console.log(formDate);
+    event.preventDefault();
+  };
+
   return(
     <div>
       <h4>Give a comment!</h4>
-      <form>
-        <input placeholder="username" type="text" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">UserName</label>
+        <input 
+        placeholder="username" type="text" value={formDate.username} onChange={handleSubmit} id="username">
         <br /><br />
-        <textarea name="" id="">Remarks</textarea>
+        <label htmlFor="Remark">Remark</label>
+        <textarea value={formDate.remarks}name="" id="Remark" onChange={handleSubmit}>Remarks</textarea >
         <br /><br />
-        <input  placeholder="rating" type="number" />
+        <label htmlFor="rating">Rating</label>
+        <input  placeholder="rating" type="number" min ={1} max={5} value={formDate.rating}  onChange={handleSubmit} id="rating"/>
         <br /><br />
-        <button>Submit</button>
+        <button>Add Commit</button>
       </form>
     </div>
-  )
+  );
 }
