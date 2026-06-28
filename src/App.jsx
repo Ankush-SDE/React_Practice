5ac3d70b846a3134434de55363376612a6474cb0
Ankush Kumar <ankush1524kumar@gmail.com>
2026-06-28 17:22:03 +0530

    add Comment

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import './App.css'
 // import LudoBoard from './LudoBoard';
 // import TodoList from './TodoList';
 // import Form from './Form';
-import CommentForm from './CommnetsForm';
+//import CommentForm from './CommnetsForm';
+import Comment from './Comment';
 function App() {
   return (
   <>
-  <CommentForm/>
+  <Comment/>
     </>
   );
 }
--- src/CommnetsForm.jsx
+++ src/CommnetsForm.jsx
@@ -1,39 +1,109 @@
-import { useState } from "react"
+// import { useState } from "react"
 
-export default function CommentForm(){
-  let [formDate,setFormDate] = useState({
-    username:"",
-    remarks:"",
-    rating:5
+// export default function CommentForm(){
+//   let [formDate,setFormDate] = useState({
+//     username:"",
+//     remarks:"",
+//     rating:5
+//   });
+//   let handleInputChange = (event)=>{
+//     setFormDate((currDate)=>{
+//       return{
+//         ...currDate,
+//         [event.target.name]:event.target.value,
+//       };
+//     });
+//   };
+//   let handleSubmit =(event)=>{
+//     console.log(formDate);
+//     event.preventDefault();
+//   };
+
+//   return(
+//     <div>
+//       <h4>Give a comment!</h4>
+//       <form onSubmit={handleSubmit}>
+//         <label htmlFor="username">UserName</label>
+//         <input 
+//         placeholder="username" type="text" value={formDate.username} onChange={handleSubmit} id="username" name="username">
+//         <br /><br />
+//         <label htmlFor="remarks">Remark</label>
+//         <textarea value={formDate.remarks}name="" id="remarks" onChange={handleSubmit} name ="remarks">Remarks</textarea >
+//         <br /><br />
+//         <label htmlFor="rating">Rating</label>
+//         <input  placeholder="rating" type="number" min ={1} max={5} value={formDate.rating}  onChange={handleSubmit} id="rating" name="rating"/>
+//         <br /><br />
+//         <button>Add Commit</button>
+        
+//       </form>
+//     </div>
+//   }
+//   )
+import { useState } from "react";
+
+export default function CommentForm() {
+  const [formDate, setFormDate] = useState({
+    username: "",
+    remarks: "",
+    rating: 5,
   });
-  let handleInputChange = (event)=>{
-    setFormDate((currDate)=>{
-      return{
-        ...currDate,
-        [event.target.name]:event.target.value,
-      };
-    });
-  };
-  let handleSubmit =(event)=>{
-    console.log(formDate);
-    event.preventDefault();
+
+  const handleInputChange = (event) => {
+    setFormDate((currData) => ({
+      ...currData,
+      [event.target.name]: event.target.value,
+    }));
   };
 
-  return(
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formDate);
+  };
+
+  return (
     <div>
       <h4>Give a comment!</h4>
+
       <form onSubmit={handleSubmit}>
         <label htmlFor="username">UserName</label>
-        <input 
-        placeholder="username" type="text" value={formDate.username} onChange={handleSubmit} id="username">
-        <br /><br />
-        <label htmlFor="Remark">Remark</label>
-        <textarea value={formDate.remarks}name="" id="Remark" onChange={handleSubmit}>Remarks</textarea >
-        <br /><br />
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="username"
+          value={formDate.username}
+          onChange={handleInputChange}
+        />
+
+        <br />
+        <br />
+
+        <label htmlFor="remarks">Remark</label>
+        <textarea
+          id="remarks"
+          name="remarks"
+          value={formDate.remarks}
+          onChange={handleInputChange}
+        />
+
+        <br />
+        <br />
+
         <label htmlFor="rating">Rating</label>
-        <input  placeholder="rating" type="number" min ={1} max={5} value={formDate.rating}  onChange={handleSubmit} id="rating"/>
-        <br /><br />
-        <button>Add Commit</button>
+        <input
+          type="number"
+          id="rating"
+          name="rating"
+          min={1}
+          max={5}
+          value={formDate.rating}
+          onChange={handleInputChange}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">Add Comment</button>
       </form>
     </div>
   );
